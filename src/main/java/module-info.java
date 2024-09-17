b9ecdbb6c53d619004d632845d42453a1f60f823
noamfav <noam.favier@icloud.com>
2024-09-17 21:36:57 +0200

    new file:   .classpath 	modified:   .factorypath 	new file:   .project 	modified:   .settings/org.eclipse.jdt.core.prefs 	modified:   pom.xml 	modified:   src/main/java/com/um_project_game/Launcher.java 	modified:   src/main/java/module-info.java 	modified:   src/test/java/com/um_project_game/LauncherTest.java 	modified:   target/classes/com/um_project_game/Launcher.class 	modified:   target/classes/module-info.class 	new file:   target/test-classes/com/um_project_game/LauncherTest.class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,11 +5,13 @@ module com.um_project_game {
     requires org.apache.logging.log4j;
     requires javafx.controls;
     requires javafx.web;
+    requires transitive javafx.graphics;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
+    opens com.um_project_game to javafx.graphics;
     exports com.um_project_game;
 }
