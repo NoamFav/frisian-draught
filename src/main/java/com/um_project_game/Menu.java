ebdf4806b01470679f5dc2ee723453bcf0c0b2a4
noamfav <noam.favier@icloud.com>
2024-09-18 18:23:44 +0200

    auto-commit

--- src/main/java/com/um_project_game/Menu.java
+++ src/main/java/com/um_project_game/Menu.java
@@ -1,10 +1,21 @@
 package com.um_project_game;
 
+import com.um_project_game.util.Buttons;
 import com.um_project_game.util.Colors;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 public class Menu {
 
-    public Menu() {
-        System.out.println("Menu");
+    public Menu(Pane root) {
+        Buttons startGameButton = new Buttons("Start Game", 200, 100, 200, 50);
+        Buttons exitButton = new Buttons("Exit", 200, 200, 200, 50);
+        
+        exitButton.getButton().setOnAction(e -> {
+            System.exit(0);
+        });
+
+        root.getChildren().addAll(startGameButton.getButton(), exitButton.getButton());
     }
 }
--- src/main/java/com/um_project_game/util/Buttons.java
+++ src/main/java/com/um_project_game/util/Buttons.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 
 public class Buttons {
 
+    private Button button;
+
     /**
      * @param text The text to be displayed on the button
      * @param x The x-coordinate of the button
@@ -12,13 +14,14 @@ public class Buttons {
      * @param height The height of the button
      */
     public Buttons(String text, int x, int y, int width, int height) {
-        Button button = new Button(text);
+        button = new Button(text);
         button.setLayoutX(x);
         button.setLayoutY(y);
         button.setPrefWidth(width);
         button.setPrefHeight(height);
-        button.setStyle("-fx-background-color: " + Colors.colors.get("buttonColor") + "; -fx-text-fill: " + Colors.colors.get("buttonTextColor") + ";");
-        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: " + Colors.colors.get("buttonHoverColor") + "; -fx-text-fill: " + Colors.colors.get("buttonHoverTextColor") + ";"));
-        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: " + Colors.colors.get("buttonColor") + "; -fx-text-fill: " + Colors.colors.get("buttonTextColor") + ";"));
+    }
+
+    public Button getButton() {
+        return button;
     }
 }
