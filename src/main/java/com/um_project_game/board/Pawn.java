7ac176d3038c724a94b02b1824d40e75cdae226a
NoamFav <95364866+NoamFav@users.noreply.github.com>
2024-09-21 23:16:03 +0200

    added pawns, board, and created custom pawns from scratch

--- src/main/java/com/um_project_game/board/Pawn.java
+++ src/main/java/com/um_project_game/board/Pawn.java
@@ -4,16 +4,22 @@ import org.joml.Vector2i;
 
 import javafx.scene.image.Image;
 
+import com.um_project_game.util.PawnImages;
+
 public class Pawn {
 
     private Vector2i position;
     private boolean isKing;
+    private boolean isWhite;
     private Image image;
 
-    public Pawn(Vector2i position) {
+    private PawnImages pawnImages = PawnImages.getPawnImage();
+
+    public Pawn(Vector2i position, boolean isWhite) {
         this.position = position;
         this.isKing = false;
-        this.image = new Image(getClass().getResourceAsStream("/pawn.png"));
+        this.isWhite = isWhite;
+        this.image = isWhite ? pawnImages.whitePawn() : pawnImages.blackPawn();
     }
 
     public Vector2i getPosition() {
@@ -25,10 +31,19 @@ public class Pawn {
     }
 
     public boolean isKing() {
+        image = isWhite ? pawnImages.whiteKing() : pawnImages.blackKing();
         return isKing;
     }
 
     public void setKing(boolean king) {
         isKing = king;
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public Image getImage() {
+        return image;
     }
 }
