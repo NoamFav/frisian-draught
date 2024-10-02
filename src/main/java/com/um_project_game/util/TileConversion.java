35cb44a5944cb15d7b7bc7e218d6c90bd4242550
TimDanielQuiram <02quti1bif@hft-stuttgart.de>
2024-10-02 11:39:42 +0200

    Finished tile conversion methods

--- src/main/java/com/um_project_game/util/TileConversion.java
+++ src/main/java/com/um_project_game/util/TileConversion.java
@@ -20,7 +20,13 @@ public class TileConversion {
         return remainder == 0 ?  9 : (remainder*2) - 1;
     }
 
+    // returns the vector from portable draughts notation
     public static Vector2i getVector(int tile) {
         return new Vector2i(getRow(tile),getCol(tile));
+    }
+
+    // returns the tile in portable draughts notation ( 1 through 50 )
+    public static int getTileNotation(Vector2i vector2i) {
+        return vector2i.x * 5 + vector2i.y / 2 + 1;
     }
 }
