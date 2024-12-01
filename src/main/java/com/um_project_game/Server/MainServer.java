8b5859614bfa0db124a51be8f8ec2a693740d628
noamfav <noam.favier@icloud.com>
2024-12-01 22:35:20 +0100

    import fix

--- src/main/java/com/um_project_game/Server/MainServer.java
+++ src/main/java/com/um_project_game/Server/MainServer.java
@@ -1,7 +1,13 @@
 package com.um_project_game.Server;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class MainServer implements Runnable {
 
